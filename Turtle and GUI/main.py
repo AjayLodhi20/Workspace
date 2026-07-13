83f49a80bda24b21792beffa69ca1de7a347cc80
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-13 17:49:02 +0530

    starting

---
+++ Turtle and GUI/main.py
@@ -0,0 +1,31 @@
+import random
+from turtle import Turtle, Screen
+import random
+tim = Turtle()
+color = ["blue", 'red', 'yellow']
+for sides in range(3, 11):
+    for i in range(sides):
+        tim.color(random.choice(color))
+        tim.forward(70)
+        tim.right(360/sides)
+
+
+screen = Screen()
+screen.exitonclick()
+
+# tim.shape('turtle')
+# tim.color("sky blue")
+# for i in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+#     tim.fd(100)
+
+# tim.pensize(2)
+# tim.speed(5)
+#
+# for i in range(15):
+#     tim.pendown()
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#
---
+++ Turtle and GUI/random_walk.py
@@ -0,0 +1,31 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+
+tim.width(10)
+tim.speed(1)
+turtle.colormode(255)
+
+def get_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r,g,b)
+
+while True:
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
