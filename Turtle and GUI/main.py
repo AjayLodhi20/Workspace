715ae0c81ad50703fc38f1adf315fa1fdbf6eb26
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-14 12:40:12 +0530

    cool patterns

--- Turtle and GUI/main.py
+++ Turtle and GUI/main.py
@@ -1,31 +1,45 @@
+# import colorgram
+# rgb_colors = []
+# colors = colorgram.extract("Addictive---detail-of-Dam-007.jpg", 20)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+import turtle as turtle_module
+import colorgram
 import random
-from turtle import Turtle, Screen
-import random
-tim = Turtle()
-color = ["blue", 'red', 'yellow']
-for sides in range(3, 11):
-    for i in range(sides):
-        tim.color(random.choice(color))
-        tim.forward(70)
-        tim.right(360/sides)
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.penup()
+tim.hideturtle()
+
+tim.speed("fastest")
+color_list = [(245, 243, 239), (247, 242, 244), (204, 164, 107), (239, 245, 241), (155, 73, 46), (235, 238, 244), (52, 92, 123), (224, 201, 135), (171, 153, 40), (138, 31, 21), (132, 162, 185), (200, 91, 71), (48, 122, 87), (14, 99, 73), (95, 73, 75), (146, 178, 147), (72, 47, 38), (163, 142, 158), (234, 175, 165), (55, 46, 50)]
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
 
 
-screen = Screen()
+for dot_count in range(1, number_of_dots+1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+screen = turtle_module.Screen()
 screen.exitonclick()
-
-# tim.shape('turtle')
-# tim.color("sky blue")
-# for i in range(4):
-#     tim.forward(100)
-#     tim.right(90)
-#     tim.fd(100)
-
-# tim.pensize(2)
-# tim.speed(5)
-#
-# for i in range(15):
-#     tim.pendown()
-#     tim.forward(10)
-#     tim.penup()
-#     tim.forward(10)
-#
