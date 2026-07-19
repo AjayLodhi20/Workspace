dd30399ca282943260d2947802e97f974c8ff498
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-19 14:04:06 +0530

    move both paddle

--- The pong game/main.py
+++ The pong game/main.py
@@ -11,15 +11,18 @@ paddle2 = Paddle()
 screen.tracer(0)
 paddle1.create_paddle(x=350, y=0)
 paddle2.create_paddle(x= -350, y = 0)
+
 screen.listen()
-
-
 screen.onkey(paddle1.move_up, 'Up')
-screen.onkey(paddle2.move_up, 'w')
 screen.onkey(paddle1.move_down, 'Down')
+
+screen.onkey(paddle2.move_up, 'w')
 screen.onkey(paddle2.move_down, 's')
 
-screen.update()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
--- The pong game/paddle.py
+++ The pong game/paddle.py
@@ -1,5 +1,4 @@
 from turtle import Turtle
-
 class Paddle(Turtle):
     def __init__(self):
         super().__init__()
@@ -10,15 +9,19 @@ class Paddle(Turtle):
         self.penup()
         self.color("white")
         self.goto(x,y)
-        self.shapesize(stretch_wid=4, stretch_len=1)
+        self.shapesize(stretch_wid=5, stretch_len=1)
 
     def move_up(self):
-        self.setheading(90)
-        self.forward(10)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
 
     def move_down(self):
-        self.setheading(270)
-        self.forward(10)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+
 
 
 
