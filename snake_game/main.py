1a432df0140231145bc50c21a84cb502e1e16bed
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-19 11:09:04 +0530

    snake

---
+++ snake_game/main.py
@@ -0,0 +1,45 @@
+# TODO: create a snake body
+from turtle import Screen, Turtle
+import time
+from food import Food
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height= 600)
+screen.bgcolor("black")
+screen.title("My snake game")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+# TODO: move the snake
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+# detect collision with food
+    if snake.head.distace(food) <15:
+        food.refresh()
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
