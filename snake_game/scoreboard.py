47e3b1e12696ecc149ba3d16109a3e5504f2dd84
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-24 12:33:54 +0530

    adding high score function

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_Score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -12,15 +13,21 @@ class Scoreboard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
         self.write(f"score: {self.score}", align="center", font = FONT )
 
 
     def update_scoreboard(self):
-        self.write(f"score: {self.score}", align="center", font = FONT)
+        self.clear()
+        self.write(f"score: {self.score} High Score: {self.high_Score}", align="center", font = FONT)
 
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align="center", font=FONT)
+    def reset(self):
+        if self.score > self.high_Score:
+            self.high_Score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align="center", font=FONT)
