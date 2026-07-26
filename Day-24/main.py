ead538c03fec9fb6b588a96727cf1ab8bbae349c
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-26 18:28:00 +0530

    hello world

--- Day-24/main.py
+++ Day-24/main.py
@@ -3,9 +3,9 @@
 # print(contents)
 # file.close()
 
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+with open("../../OneDrive/Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
 
 # with open("my_file.txt", mode="w") as file:
 #     file.write("new text.")
