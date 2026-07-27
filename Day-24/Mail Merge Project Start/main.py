2785977e40c5a34abc4f7fa3c8a0b57868056faf
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-27 12:20:11 +0530

    change the name and save every letter in different file

--- Day-24/Mail Merge Project Start/main.py
+++ Day-24/Mail Merge Project Start/main.py
@@ -1,9 +1,13 @@
-#TODO: Create a letter using starting_letter.txt
+PLACEHOLDER = "[name]"
 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+    print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_names = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_names)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_names}.txt", mode="w") as completed_letters:
+            completed_letters.write(new_letter)
