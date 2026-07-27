8db8b1d1129a76edfeb9e35c8e7a2370488db8a7
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-27 13:59:40 +0530

    pandas tutorial

--- Day25/main.py
+++ Day25/main.py
@@ -17,5 +17,42 @@
 
 import pandas
 data = pandas.read_csv("weather_data.csv")
-print(data)
+# print(type(data))
 print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# # avg = round((sum(temp_list)/len(temp_list)), 2)
+# # print(avg)
+#
+# print(data["temp"].mean())
+#
+# print(data["temp"].max())
+#
+# # get datas in columns
+# print(data["condition"])
+# # or
+# print(data.condition)
+#
+# # get datas which are in rows
+# print(data[data.day == "Monday"])
+#
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+# monday_temp = monday.temp[0]
+# print(monday_temp)
+# print((monday_temp * 1.8) + 32)
+
+# create a df from scratch
+
+data_dict = {
+    "students": ["amy", "james", "angela"]
+    "scores": [76, 56, 65]
+}
+
