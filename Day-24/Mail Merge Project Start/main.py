070a25d3a2d319ea6b948212404783c659f86d4a
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-07-27 11:57:22 +0530

    wrong code

--- Day-24/Mail Merge Project Start/main.py
+++ Day-24/Mail Merge Project Start/main.py
@@ -1,33 +1,5 @@
 #TODO: Create a letter using starting_letter.txt
 
-file = open("./Output/ReadyToSend/example.txt")
-message = file.readlines()
-
-invites = open("./Input/Names/invited_names.txt")
-names = invites.readlines()
-
-filtered_message = [i.strip() for i in message]
-
-i = 0
-for i, name in enumerate(filtered_message):
-    filtered_message.remove(name)
-    i += 2
-
-print(filtered_message)
-
-filtered_names = []
-for name in names:
-    txt = name.strip()
-    filtered_names.append(txt)
-
-print(filtered_names)
-
-
-for txt in filtered_names:
-    for i in filtered_message:
-        x = i.replace(txt, "Aang")
-        
-print(x)
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
