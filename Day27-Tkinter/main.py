b63e8b0608b8c98f112f7c1d080cfd9edc2718e1
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-08-01 23:45:34 +0530

    done

--- Day27-Tkinter/main.py
+++ Day27-Tkinter/main.py
@@ -1,27 +1,32 @@
 from tkinter import *
 
 window = Tk()
-window.minsize(width=400, height=200)
-window.title("Mile to Km converter")
+window.title("Miles to Km converter")
+window.config(pady=20, padx=20)
 # is equal to label
 equal_label = Label(text="is equal to")
-equal_label.config(padx=100, pady=100)
-equal_label.grid()
+equal_label.grid(column=0, row=1)
 
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
 # calculate kms label
+
+km_result_label = Label(text="0")
+km_result_label.grid(column=1, row=1)
 #
-km_label = Label()
+km_label = Label(text="km")
+km_label.grid(column=2, row=1)
 
 def km_calculate():
-    number = input.get()
-    number = int(number)
-    km = int(number * 1.6)
-    km_label.config((km))
+    miles = float(input.get())
+    km = miles * 1.609
+    km_result_label.config(text=f"{km}")
+
 
 button = Button(text="Calculate",command=km_calculate)
-button.grid(column=1, row=1)
+button.grid(column=1, row=2)
 
 input = Entry(width=20)
-input.grid(column= 2, row=3)
+input.grid(column= 1, row=0)
 
 window.mainloop()
