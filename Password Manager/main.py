d5e56f9a99c60144c589b61653b87f9b8729e60b
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-08-07 15:06:32 +0530

    add password to the file

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,7 +1,24 @@
 from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or password == 0:
+        messagebox.showinfo(title="oops", message="please make sure you haven't left any fields empty")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password}\n"
+                                                      f"Is it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -28,13 +45,13 @@ website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2,column=1, columnspan=2)
 email_entry.insert(0, "satyamlodhi122@gmail.com")
-password_label = Entry(width=21)
-password_label.grid(row=3, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 # buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="add", width=36)
+add_button = Button(text="add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
