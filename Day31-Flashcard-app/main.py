e804242901f2e9a751d21a8a15a2f661ad9a24b8
Ajay Lodhi <satyamlodhi122@gmail.com>
2026-08-11 20:10:18 +0530

    UI setup of flashcard

--- Day31-Flashcard-app/main.py
+++ Day31-Flashcard-app/main.py
@@ -1,2 +1,48 @@
+from tkinter import *
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("french_vocabulary.csv")
+to_learn = data.to_dict(orient = "records")
+print(to_learn)
+
+
+
+def next_card():
+    random.choice(to_learn)
+
+
+
+window = Tk()
+window.title("Flashy")
+window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
+
+tick_img = PhotoImage(file="./images/right.png")
+wrong_img = PhotoImage(file="./images/wrong.png")
+card_front = PhotoImage(file="./images/card_front.png")
+card_back = PhotoImage(file="./images/card_back.png")
+
+canvas = Canvas(width=800, height=526)
+canvas.create_image(400, 263, image= card_front)
+
+
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text= "word", font=("Ariel", 60, "bold"))
+
+canvas.config(bg= BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0 , row= 0, columnspan=2)
+
+
+unknown_button = Button(image=wrong_img, command=next_card)
+unknown_button.grid(row=1, column=0)
+known_button = Button(image=tick_img, command= next_card)
+known_button.grid(row=1, column=1)
+
+
+
+
+
+window.mainloop()
 
